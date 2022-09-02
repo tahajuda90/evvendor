3094f734d61c440ae41ce7d54a240a8b556d4e9e
Tahajuda <tahajuda@tahajuda-X441UV>
2022-09-02 13:43:26 +0700

    revisi logo dan nama

--- application/views/component/header.php
+++ application/views/component/header.php
@@ -1,4 +1,4 @@
-<header class="app-header"><a class="app-header__logo" href="#">Si-EMon</a>
+<header class="app-header"><a class="app-header__logo" href="#">SiPP</a>
       <!-- Sidebar toggle button--><a class="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
       <!-- Navbar Right Menu-->
       <ul class="app-nav">
